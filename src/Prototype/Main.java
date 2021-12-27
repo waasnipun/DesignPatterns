ae54a8a400e7ee48b138ec23b3f68bfc9eae9034
Nipun Waas <waasnipun@gmail.com>
2021-12-27 21:07:51 +0530

    Prototype Pattern added

---
+++ src/Prototype/Main.java
@@ -0,0 +1,54 @@
+package Prototype;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+class Vehicle implements Cloneable{
+    private List<String> vehicleList;
+
+    public Vehicle(){
+        this.vehicleList = new ArrayList<String>();
+    }
+
+    public Vehicle(List<String> list){
+        this.vehicleList = list;
+    }
+
+    public void insertData(){
+        vehicleList.add("Honda");
+        vehicleList.add("Civic");
+        vehicleList.add("Hyundai");
+    }
+
+    public List<String> getVehicleList(){
+        return this.vehicleList;
+    }
+
+
+    @Override
+    public Vehicle clone() {
+        List<String> temp = new ArrayList<String>();
+        temp.addAll(this.getVehicleList());
+        return new Vehicle(temp);
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Vehicle a = new Vehicle();
+        a.insertData();
+
+        Vehicle b = (Vehicle) a.clone();
+        List<String> list = b.getVehicleList();
+        list.add("New HONDA");
+
+        System.out.println(a.getVehicleList());
+        System.out.println(list);
+
+        b.getVehicleList().remove("Civic");
+
+        System.out.println(a.getVehicleList());
+        System.out.println(list);
+    }
+}
