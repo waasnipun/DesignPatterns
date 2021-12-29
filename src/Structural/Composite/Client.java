a523ad2bcdde9c848e7c8487424a1396f8ff85d3
Nipun Waas <waasnipun@gmail.com>
2021-12-29 08:38:12 +0530

    Composite pattern Added

---
+++ src/Structural/Composite/Client.java
@@ -0,0 +1,76 @@
+package Structural.Composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Account{
+    public abstract float getBalance();
+}
+
+class DepositAccount extends Account{
+    private String accountNo;
+    private float accountBalance;
+
+    public DepositAccount(String accountNo, float accountBalance){
+        super();
+        this.accountBalance = accountBalance;
+        this.accountNo = accountNo;
+    }
+
+    @Override
+    public float getBalance() {
+        return accountBalance;
+    }
+}
+
+class SavingsAccount extends Account{
+    private String accountNo;
+    private float accountBalance;
+
+    public SavingsAccount(String accountNo, float accountBalance){
+        super();
+        this.accountBalance = accountBalance;
+        this.accountNo = accountNo;
+    }
+
+    @Override
+    public float getBalance() {
+        return accountBalance;
+    }
+}
+
+class CompositeAccount extends Account{
+
+    private float totalBalance;
+    private List<Account> accountList = new ArrayList<Account>();
+
+    @Override
+    public float getBalance() {
+        totalBalance = 0;
+        for (Account f: accountList){
+            totalBalance = totalBalance + f.getBalance();
+        }
+        return totalBalance;
+    }
+    public void addAccount(Account acc){
+        accountList.add(acc);
+    }
+    public void removeAccount(Account acc){
+        accountList.remove(acc);
+    }
+}
+
+
+
+public class Client {
+    public static void main(String[] args) {
+        CompositeAccount component = new CompositeAccount();
+
+        component.addAccount(new DepositAccount("DA001",100));
+        component.addAccount(new DepositAccount("DA002",300));
+        component.addAccount(new SavingsAccount("SA001",1000));
+
+        float totalBalance = component.getBalance();
+        System.out.println("Total Balance: "+totalBalance);
+    }
+}
--- src/Structural/Composite/Main.java
+++
@@ -1,4 +0,0 @@
-package Structural.Composite;
-
-public class Main {
-}
