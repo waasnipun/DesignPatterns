ae7ef59532b0294c2eafde26a61785df253bc1e2
Nipun Waas <waasnipun@gmail.com>
2021-12-30 12:15:43 +0530

    flyweight pattern added

--- src/Structural/Flyweight/Main.java
+++ src/Structural/Flyweight/Main.java
@@ -1,5 +1,8 @@
 package Structural.Flyweight;
 
+import java.util.HashMap;
+import java.util.Random;
+
 interface Employee{
     public void assignSkill(String skill);
     public void task();
@@ -45,13 +48,58 @@ class Tester implements Employee{
     }
 }
 
-
+//Minimizes the object creation
 class EmployeeFactory{
+    private static HashMap<String, Employee> m = new HashMap<String, Employee>();
 
+    public static Employee getEmployee(String type){
+        Employee p = null;
+        if(m.get(type)!=null){
+            p = m.get(type);
+        }
+        else{
+            switch (type){
+                case "Developer":
+                    System.out.println("Developer created!");
+                    p = new Developer();
+                    break;
+                case "Tester":
+                    System.out.println("Tester created!");
+                    p = new Tester();
+                    break;
+                default:
+                    System.out.println("No such Employee!");
+            }
+            m.put(type,p);
+        }
+        return p;
+    }
 }
 
+//client method
 public class Main {
-    public static void main(String[] args) {
 
+    private static String employeeType[] = {"Developer", "Tester"};
+    private static String skills[] = {"Java", "C++", "Python", ".Net"};
+
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            Employee e = EmployeeFactory.getEmployee(getRandEmployee());
+            e.assignSkill(getRandSkill());
+            e.task();
+        }
     }
+
+    public static String getRandEmployee(){
+        Random r = new Random();
+        int randInt = r.nextInt(employeeType.length);
+        return employeeType[randInt];
+    }
+
+    public static String getRandSkill(){
+        Random r = new Random();
+        int randInt = r.nextInt(skills.length);
+        return skills[randInt];
+    }
+
 }
